f2a7528aea8843afc5f67c4f2f959447b89bd342
David Kirkby <dkirkby@uci.edu>
2014-10-28 21:30:49 -0700

    Add H(z)/(1+z) plot

--- plot.py
+++ plot.py
@@ -16,6 +16,8 @@ def main():
         help = 'name of input file to read (extension .npz will be added)')
     parser.add_argument('--posteriors',type=int, default = 0,
         help = 'posteriors permutation index to use')
+    parser.add_argument('--zmax', type = float, default = 3.0,
+        help = 'maximum redshift to display on H(z)/(1+z) plot')
     parser.add_argument('--save', type = str, default = None,
         help = 'name of plot to save')
     args = parser.parse_args()
@@ -48,10 +50,16 @@ def main():
     DA_limits = gphist.analysis.calculate_confidence_limits(
         DA_hist[args.posteriors],levels,bin_range)
 
-    fig = plt.figure(figsize=(14,6))
+    # Calculate the corresponding limits on acceleration H(z)/(1+z).
+    accel_limits = 299792.458/DH_limits/DH0/(1+zevol)
+
+    # Find first z index beyond H(z)/(1+z) plot.
+    iend = 1+np.argmax(zevol > args.zmax)
+
+    fig = plt.figure(figsize=(14,12))
     fig.set_facecolor('white')
 
-    plt.subplot(1,2,1)
+    plt.subplot(2,2,1)
     plt.xscale('log')
     plt.grid(True)
     plt.xlim([1.,1000.])
@@ -63,7 +71,7 @@ def main():
     plt.xlabel('log(1+z)')
     plt.ylabel('DH(z)/DH0(z)')
 
-    plt.subplot(1,2,2)
+    plt.subplot(2,2,2)
     plt.xscale('log')
     plt.grid(True)
     plt.xlim([1.,1000.])
@@ -75,15 +83,28 @@ def main():
     plt.xlabel('log(1+z)')
     plt.ylabel('DA(z)/DA0(z)')
 
+    plt.subplot(2,2,3)
+    plt.xscale('linear')
+    plt.grid(True)
+    plt.xlim([0.,args.zmax])
+    plt.plot(zevol[:iend],accel_limits[2,:iend])
+    plt.fill_between(zevol[:iend],accel_limits[0,:iend],accel_limits[-1,:iend],
+        facecolor='blue',alpha=0.25)
+    plt.plot(zevol[:iend],accel_limits[0,:iend],'b--')
+    plt.plot(zevol[:iend],accel_limits[1,:iend],'b-')
+    plt.plot(zevol[:iend],accel_limits[2,:iend],'b--')
+    plt.xlabel('z')
+    plt.ylabel('H(z)/(1+z)')
+
     if args.save:
         plt.savefig(args.save)
-
+    '''
     plt.figure(figsize=(14,6))
     plt.subplot(1,2,1)
     plt.plot(DH_hist[args.posteriors,0])
     plt.subplot(1,2,2)
     plt.plot(DA_hist[args.posteriors,-1])
-
+    '''
     plt.show()
 
 if __name__ == '__main__':
