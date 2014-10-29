6d1a8a84be798d756095d0a66e478b8e385904c7
David Kirkby <dkirkby@uci.edu>
2014-10-28 20:57:13 -0700

    Rename priors to posteriors in plot program

--- plot.py
+++ plot.py
@@ -14,8 +14,8 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--input',type = str, default = None,
         help = 'name of input file to read (extension .npz will be added)')
-    parser.add_argument('--priors',type=int, default = 0,
-        help = 'priors permutation index to use')
+    parser.add_argument('--posteriors',type=int, default = 0,
+        help = 'posteriors permutation index to use')
     parser.add_argument('--save', type = str, default = None,
         help = 'name of plot to save')
     args = parser.parse_args()
@@ -32,18 +32,19 @@ def main():
     posterior_names = loaded['posterior_names']
 
     npost = len(posterior_names)
-    if args.priors < 0 or args.priors >= 2**npost:
-        print 'Invalid priors %d (should be in the range 0 - %d)' % (args.priors,2**npost-1)
+    if args.posteriors < 0 or args.posteriors >= 2**npost:
+        print 'Invalid posteriors %d (should be in the range 0 - %d)' % (
+            args.posteriors,2**npost-1)
         return -1
     perms = gphist.analysis.get_permutations(npost)
-    priors_name = '+'.join(posterior_names[perms[args.priors]]) or 'None'
-    print '%d = %s' % (args.priors,priors_name)
+    posteriors_name = '+'.join(posterior_names[perms[args.posteriors]]) or 'None'
+    print '%d = %s' % (args.posteriors,posteriors_name)
 
     levels = np.array([0.05,0.5,0.95])
     DH_limits = gphist.analysis.calculate_confidence_limits(
-        DH_hist[args.priors],levels,bin_range)
+        DH_hist[args.posteriors],levels,bin_range)
     DA_limits = gphist.analysis.calculate_confidence_limits(
-        DA_hist[args.priors],levels,bin_range)
+        DA_hist[args.posteriors],levels,bin_range)
 
     fig = plt.figure(figsize=(14,6))
     fig.set_facecolor('white')
@@ -77,9 +78,9 @@ def main():
 
     plt.figure(figsize=(14,6))
     plt.subplot(1,2,1)
-    plt.plot(DH_hist[args.priors,0])
+    plt.plot(DH_hist[args.posteriors,0])
     plt.subplot(1,2,2)
-    plt.plot(DA_hist[args.priors,-1])
+    plt.plot(DA_hist[args.posteriors,-1])
 
     plt.show()
 
