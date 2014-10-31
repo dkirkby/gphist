4e2adb08110f7504127d87b9dca8712650f30f9a
David Kirkby <dkirkby@uci.edu>
2014-10-31 15:12:58 -0700

    Add support for combining histograms from multiple input files.

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@
 """
 
 import argparse
+import glob
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +14,7 @@ def main():
     # Parse command-line arguments.
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--input',type = str, default = None,
-        help = 'name of input file to read (extension .npz will be added)')
+        help = 'name of input file(s) to read (wildcard patterns are supported)')
     parser.add_argument('--posteriors',type=int, default = 0,
         help = 'posteriors permutation index to use')
     parser.add_argument('--level', type = float, default = 0.9,
@@ -30,27 +31,47 @@ def main():
         print 'Missing required input arg.'
         return -1
     show_examples = not args.no_examples
-
-    loaded = np.load(args.input+'.npz')
-    DH_hist = loaded['DH_hist']
-    DA_hist = loaded['DA_hist']
-    DH0 = loaded['DH0']
-    DA0 = loaded['DA0']
-    zevol = loaded['zevol']
-    bin_range = loaded['bin_range']
-    DH_realizations = loaded['DH_realizations']
-    DA_realizations = loaded['DA_realizations']
-    posterior_names = loaded['posterior_names']
-
-    npost = len(posterior_names)
-    if args.posteriors < 0 or args.posteriors >= 2**npost:
-        print 'Invalid posteriors %d (should be in the range 0 - %d)' % (
-            args.posteriors,2**npost-1)
+    input_files = glob.glob(args.input)
+    if not input_files:
+        input_files = glob.glob(args.input + '.npz')
+    if not input_files:
+        print 'No input files match the pattern %r' % args.input
         return -1
-    perms = gphist.analysis.get_permutations(npost)
-    posteriors_name = '+'.join(posterior_names[perms[args.posteriors]]) or 'None'
-    print '%d = %s' % (args.posteriors,posteriors_name)
 
+    # Combine the histograms from each input file.
+    for index,input_file in enumerate(input_files):
+        loaded = np.load(input_file)
+        if index == 0:
+            DH_hist = loaded['DH_hist']
+            DA_hist = loaded['DA_hist']
+            DH0 = loaded['DH0']
+            DA0 = loaded['DA0']
+            zevol = loaded['zevol']
+            bin_range = loaded['bin_range']
+            if show_examples:
+                DH_realizations = loaded['DH_realizations']
+                DA_realizations = loaded['DA_realizations']
+            posterior_names = loaded['posterior_names']
+            npost = len(posterior_names)
+            if args.posteriors < 0 or args.posteriors >= 2**npost:
+                print 'Invalid posteriors %d (should be in the range 0 - %d)' % (
+                    args.posteriors,2**npost-1)
+                return -1
+            perms = gphist.analysis.get_permutations(npost)
+            posteriors_name = '-'.join(posterior_names[perms[args.posteriors]]) or 'Prior'
+            print '%d = %s' % (args.posteriors,posteriors_name)
+        else:
+            assert np.array_equal(DH0,loaded['DH0']),'Found inconsistent DH0'
+            assert np.array_equal(DA0,loaded['DA0']),'Found inconsistent DA0'
+            assert np.array_equal(zevol,loaded['zevol']),'Found inconsistent zevol'
+            assert np.array_equal(bin_range,loaded['bin_range']),\
+                'Found inconsistent bin_range'
+            assert np.array_equal(posterior_names,loaded['posterior_names']),\
+                'Found inconsistent posterior_names'
+            DH_hist += loaded['DH_hist']
+            DA_hist += loaded['DA_hist']
+
+    # Calculate the confidence bands from the combined histograms.
     DH_limits = gphist.analysis.calculate_confidence_limits(
         DH_hist[args.posteriors],[args.level],bin_range)
     DA_limits = gphist.analysis.calculate_confidence_limits(
@@ -59,12 +80,13 @@ def main():
     # Calculate the corresponding limits and realizations acceleration H(z)/(1+z).
     clight = 299792.458
     accel_limits = clight/DH_limits/DH0/(1+zevol)
-    accel_realizations = clight/DH_realizations[args.posteriors]/(1+zevol)
+    if show_examples:
+        accel_realizations = clight/DH_realizations[args.posteriors]/(1+zevol)
 
     # Find first z index beyond H(z)/(1+z) plot.
     iend = 1+np.argmax(zevol > args.zmax)
 
-    fig = plt.figure(figsize=(12,8))
+    fig = plt.figure(posteriors_name,figsize=(12,8))
     fig.subplots_adjust(left=0.06,bottom=0.07,right=0.98,top=0.99,wspace=0.15,hspace=0.18)
     fig.set_facecolor('white')
 
